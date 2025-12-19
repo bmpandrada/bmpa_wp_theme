7f2e412c391baec98f65d49bfbc7c725839cbeff
bruce <bmpandrada@gmail.com>
2025-12-19 10:39:26 +0800

    fix: add dynamic banner, title, subitle, in  ACF

--- themes/bmpa_two_themes/functions.php
+++ themes/bmpa_two_themes/functions.php
@@ -1,5 +1,34 @@
 <?php
 
+// includes
+function pageBanner($args = [])
+{
+  if (!isset($args['title'])) {
+    $args['title'] = get_the_title();
+  }
+  if (!isset($args['subtitle'])) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+  if (!isset($args['photo'])) {
+    if (get_field('page_banner_background_image') and !is_archive() and !is_home()) {
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+  }
+?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo'] ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle'] ?></p>
+      </div>
+    </div>
+  </div>
+<?php }
+
+
 // for files
 function university_files()
 {
@@ -22,6 +51,9 @@ function university_features()
   register_nav_menu('footerMainMenuLocation', 'Footer Main Menu Location');
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 200, true);
+  add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 };
 
 add_action('after_setup_theme', 'university_features'); // navbar
